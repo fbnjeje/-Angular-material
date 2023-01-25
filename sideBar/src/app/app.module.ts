dbdb0956c83ad49332f35660d1a3176ce0d53125
Neonx <fabiangomes2015@gmail.com>
2023-01-24 20:59:41 -0500

    add first toolbar

--- sideBar/src/app/app.module.ts
+++ sideBar/src/app/app.module.ts
@@ -4,7 +4,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+// angular material
+
+import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,7 +17,9 @@ import { MatButtonModule } from "@angular/material/button";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
